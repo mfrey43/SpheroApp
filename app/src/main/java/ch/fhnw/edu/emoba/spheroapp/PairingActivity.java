9fc1d398d9886b8b017097af22012eebe1cf70e3
mfrey <mfrey43@gmail.com>
2017-05-30 15:38:15 +0200

    added min and max radius, added spheromock

--- app/src/main/java/ch/fhnw/edu/emoba/spheroapp/PairingActivity.java
+++ app/src/main/java/ch/fhnw/edu/emoba/spheroapp/PairingActivity.java
@@ -1,27 +1,70 @@
 package ch.fhnw.edu.emoba.spheroapp;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class PairingActivity extends AppCompatActivity {
+import ch.fhnw.edu.emoba.spherolib.SpheroRobotDiscoveryListener;
+import ch.fhnw.edu.emoba.spherolib.SpheroRobotFactory;
+import ch.fhnw.edu.emoba.spherolib.SpheroRobotProxy;
+
+import static ch.fhnw.edu.emoba.spherolib.SpheroRobotDiscoveryListener.SpheroRobotBluetoothNotification.Online;
+
+public class PairingActivity extends AppCompatActivity implements SpheroRobotDiscoveryListener {
+
+    TextView textView;
+    SpheroRobotProxy proxy;
+
+    boolean skip = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pairing);
+        textView = (TextView)findViewById(R.id.textView);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                launchMainActivity();
-            }
-        }, 500);
+        if(skip){
+            launchMainActivity();
+        }else{
+            boolean onEmulator = Build.PRODUCT.startsWith("sdk");
+            proxy = SpheroRobotFactory.createRobot(onEmulator);
+            proxy.setDiscoveryListener(this);
+            proxy.startDiscovering(getApplicationContext());
+        }
+
     }
 
     private void launchMainActivity(){
         startActivity(new Intent(this, MainActivity.class));
+        if(proxy != null){
+            proxy.stopDiscovering();
+        }
+    }
+
+    private void updateText(String text){
+        textView.setText(text);
+    }
+
+    @Override
+    public void handleRobotChangedState(final SpheroRobotBluetoothNotification spheroRobotBluetoothNotification) {
+        if(spheroRobotBluetoothNotification.equals(Online)){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    launchMainActivity();
+                }
+            });
+        }else{
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    updateText(spheroRobotBluetoothNotification.name());
+                }
+            });
+        }
     }
 }
--- app/src/main/java/ch/fhnw/edu/emoba/spheroapp/TouchView.java
+++ app/src/main/java/ch/fhnw/edu/emoba/spheroapp/TouchView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -14,7 +15,12 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import ch.fhnw.edu.emoba.spherolib.SpheroRobotProxy;
+import ch.fhnw.edu.emoba.spherolib.impl.SpheroMock;
+
 public class TouchView extends View {
+
+    private int deadZoneRadius = 50;
 
     private Paint linePaint;
     private Paint circlePaint;
@@ -22,7 +28,12 @@ public class TouchView extends View {
     private float currentX;
     private float currentY;
 
+    private int maxRadius;
+    private double minVelocity;
+    private double maxVelocity;
     private ScheduledFuture viewTask;
+
+    SpheroRobotProxy sphero;
 
     private AtomicBoolean isCancelled = new AtomicBoolean();
 
@@ -44,12 +55,17 @@ public class TouchView extends View {
             public void onGlobalLayout() {
                 currentX = getWidth() / 2;
                 currentY = getHeight() / 2;
+
+                int shorterEdgeLength = getWidth() < getHeight() ? getWidth() : getHeight();
+                maxRadius = shorterEdgeLength / 2;
+                minVelocity = Math.pow(deadZoneRadius, 2);
+                maxVelocity = Math.pow(maxRadius, 2);
             }
         });
     }
 
     @Override
-    public void onAttachedToWindow(){
+    public void onAttachedToWindow() {
         super.onAttachedToWindow();
 
         ScheduledExecutorService scheduler;
@@ -62,10 +78,12 @@ public class TouchView extends View {
                 postInvalidate();
             }
         }, 50, 50, TimeUnit.MILLISECONDS);
+
+        sphero = new SpheroMock();
     }
 
     @Override
-    public void onDetachedFromWindow(){
+    public void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         viewTask.cancel(true);
     }
@@ -73,8 +91,9 @@ public class TouchView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
-        canvas.drawCircle(getWidth()/2, getHeight()/2, 50, circlePaint);
-        canvas.drawLine(getWidth()/2, getHeight()/2, currentX, currentY, linePaint);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, deadZoneRadius, circlePaint);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, maxRadius, circlePaint);
+        canvas.drawLine(getWidth() / 2, getHeight() / 2, currentX, currentY, linePaint);
         canvas.drawCircle(currentX, currentY, 10, linePaint);
     }
 
@@ -82,6 +101,25 @@ public class TouchView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         currentX = event.getX();
         currentY = event.getY();
+
+        double deltaX = currentX - getWidth() / 2;
+        double deltaY = currentY - getHeight() / 2;
+        double rad = Math.atan2(-deltaX, deltaY); // start 0° at the top
+        double heading = rad * (180 / Math.PI) + 180;
+
+
+        double velocity = Math.pow(deltaX, 2) + Math.pow(deltaY, 2) - minVelocity;
+        if(velocity < minVelocity){
+            velocity = 0;
+        }
+
+        double speed = velocity / (maxVelocity - minVelocity);
+        if(speed > 1){
+            speed = 1;
+        }
+
+        sphero.drive((float)heading, (float)speed);
+
         return true;
     }
 
